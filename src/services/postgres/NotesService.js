0472ce53ae968b1799820c3149948bb80a09232c
tsaliscepu <tsaliscepu@gmail.com>
2023-07-03 06:11:02 +0700

    Membuat Fungsi getNotes dan getNoteById

--- src/services/postgres/NotesService.js
+++ src/services/postgres/NotesService.js
@@ -1,6 +1,8 @@
 const { nanoid } = require("nanoid");
 const { Pool } = require("pg");
 const InvariantError = require("../../exceptions/InvariantError");
+const { mapDBToModel } = require("../../utils");
+const NotFoundError = require("../../exceptions/NotFoundError");
 
 class NotesService {
     constructor() {
@@ -24,5 +26,24 @@ class NotesService {
         }
 
         return result.rows[0].id;
+    }
+
+    async getNotes() {
+        const result = await this._pool.query('SELECT * FROM notes');
+        return result.rows.map(mapDBToModel);
+    }
+
+    async getNoteById(id) {
+        const query = {
+            text: 'SELECT * FROM notes WHERE id = $1',
+            values: [id],
+        };
+        const result = await this._pool.query(query);
+
+        if (!result.rows.length) {
+            throw new NotFoundError('Catatan tidak ditemukan');
+        }
+
+        return result.rows.map(mapDBToModel)[0];
     }
 }
